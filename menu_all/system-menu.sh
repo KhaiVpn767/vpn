a495e7dfa5d541e5019344ecff1497c0dc735ca7
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-06-15 03:50:07 +0800

    Update system-menu.sh

--- menu_all/system-menu.sh
+++ menu_all/system-menu.sh
@@ -64,21 +64,21 @@ clear
 
 x="ok"
 
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m               ⇱ SystemAdmin MENU ⇲               \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[36m╒══════════════════════════════════════════════════╕\033[0m"
+echo -e " \e[0;41;30m               ⇱ SystemAdmin MENU ⇲               \E[0m"
+echo -e "\e[36m╘══════════════════════════════════════════════════╛\033[0m"
 echo -e "
-[\033[0;32m01\033[0m] • Backup Data VPS
-[\033[0;32m02\033[0m] • Restore Data VPS
-[\033[0;32m03\033[0m] • Webmin Menu
-[\033[0;32m04\033[0m] • Update to Latest Kernel
-[\033[0;32m05\033[0m] • Check Ram VPS
-[\033[0;32m06\033[0m] • Reboot VPS
-[\033[0;32m07\033[0m] • Speedtest VPS
+[\033[1;35m01\033[0m] • Backup Data VPS
+[\033[1;35m02\033[0m] • Restore Data VPS
+[\033[1;35m03\033[0m] • Webmin Menu
+[\033[1;35m04\033[0m] • Update to Latest Kernel
+[\033[1;35m05\033[0m] • Check Ram VPS
+[\033[1;35m06\033[0m] • Reboot VPS
+[\033[1;35m07\033[0m] • Speedtest VPS
 
-[00] • Back to Main Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
+\033[1;35m[00\033[0m] • Back to Main Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
 echo ""
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 echo -ne "Select menu : "; read x
 
