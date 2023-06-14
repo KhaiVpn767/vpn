7fb78955071918f5813981dd720cf6582214becd
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-06-15 01:52:34 +0800

    Update add-grvl.sh

--- xray/add-grvl.sh
+++ xray/add-grvl.sh
@@ -76,18 +76,18 @@ fi
 
 grvl="$(cat /root/log-install.txt | grep -w "XRAY  Vless Grpc" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m       ⇱ Add VLess GRPC ⇲          \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[36m╒═══════════════════════════════════╕\033[0m"
+echo -e " \e[0;41;30m       ⇱ Add VLess GRPC ⇲          \E[0m"
+echo -e "\e[36m╘═══════════════════════════════════╛\033[0m"
 
 		read -rp "User: " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/vlessgrpc.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
-		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		echo -e "\E[44;1;39m       ⇱ Add VLess GRPC ⇲          \E[0m"
-		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo -e "\e[36m╒═══════════════════════════════════╕\033[0m"
+		echo -e " \e[0;41;30m       ⇱ Add VLess GRPC ⇲          \E[0m"
+		echo -e "\e[36m╘═══════════════════════════════════╛\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
 			echo ""
@@ -162,18 +162,18 @@ sed -i '/#vlessgrpc$/a\### '"$user $exp"'\
 vlessgrpc="$jenis"
 systemctl restart vless-grpc.service > /dev/null 2>&1
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\E[44;1;39m       ⇱ Add VLess GRPC ⇲          \E[0m" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[36m╒═══════════════════════════════════╕\033[0m" | tee -a /etc/log-create-user.log
+echo -e " \e[0;41;30m       ⇱ Add VLess GRPC ⇲          \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[36m╘═══════════════════════════════════╛\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Expired On : ${exp}" | tee -a /etc/log-create-user.log
 echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
 echo -e "Domain : ${domain}" | tee -a /etc/log-create-user.log
 echo -e "Port : ${grvl}" | tee -a /etc/log-create-user.log
 echo -e "Id : ${uuid}" | tee -a /etc/log-create-user.log
 echo -e "Encryption : none" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Link Vless Grpc : ${vlessgrpc}" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
 
