d51dc61e83336749169639288be6c1b1083956d2
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-06-23 02:21:44 +0800

    Update ins-sshws.sh

--- ssh/ins-sshws.sh
+++ ssh/ins-sshws.sh
@@ -116,14 +116,14 @@ fi
 }
 
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m          ⇱ SSH WEBSOCKET ⇲          \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[36m╒═════════════════════════════════════╕\033[0m"
+echo -e " \e[0;41;30m          ⇱ SSH WEBSOCKET ⇲          \E[0m"
+echo -e "\e[36m╘═════════════════════════════════════╛\033[0m"
 echo ""
-echo -e " 1. Enable SSH Websocket"
-echo -e " 2. Disable SSh Websocket"
+echo -e " [\033[1;35m1\033[0m]. Enable SSH Websocket"
+echo -e " [\033[1;35m2\033[0m]. Disable SSh Websocket"
 echo ""
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 read -rp "Input Number : " -e num
 if [[ "$num" = "1" ]]; then
