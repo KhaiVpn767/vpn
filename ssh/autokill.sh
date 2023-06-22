f4ac278e23ac5780ab763b83b1f5e96eead06d25
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-06-23 02:33:13 +0800

    Update autokill.sh

--- ssh/autokill.sh
+++ ssh/autokill.sh
@@ -73,17 +73,17 @@ else
 sts="${Error}"
 fi
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m           ⇱ AUTOKILL SSH ⇲        \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[36m╒═══════════════════════════════════╕\033[0m"
+echo -e " \e[0;41;30m           ⇱ AUTOKILL SSH ⇲        \E[0m"
+echo -e "\e[36m╘═══════════════════════════════════╛\033[0m"
 echo -e "Status Autokill : $sts        "
 echo -e ""
-echo -e "[1]  AutoKill After 5 Minutes"
-echo -e "[2]  AutoKill After 10 Minutes"
-echo -e "[3]  AutoKill After 15 Minutes"
-echo -e "[4]  Turn Off AutoKill/MultiLogin"
+echo -e "[\033[1;35m1\033[0m]  AutoKill After 5 Minutes"
+echo -e "[\033[1;35m2\033[0m]  AutoKill After 10 Minutes"
+echo -e "[\033[1;35m3\033[0m]  AutoKill After 15 Minutes"
+echo -e "[\033[1;35m4\033[0m]  Turn Off AutoKill/MultiLogin"
 echo ""
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -p "Select From Options [1-4 or x] :  " AutoKill
 if [ -z $AutoKill ]; then
