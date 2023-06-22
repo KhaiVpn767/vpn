8d142f516d4d0fe00afbe74f54d5157c6937d2aa
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-06-23 02:25:15 +0800

    Update ceklim.sh

--- ssh/ceklim.sh
+++ ssh/ceklim.sh
@@ -65,13 +65,13 @@ exit 0
 fi
 
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m       ⇱ CEK USER MULTI SSH ⇲      \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[36m╒══════════════════════════════════════════╕\033[0m"
+echo -e " \e[0;41;30m       ⇱ CEK USER MULTI SSH ⇲      \E[0m"
+echo -e "\e[36m╘══════════════════════════════════════════╛\033[0m"
 if [ -e "/root/log-limit.txt" ]; then
 echo "User Who Violate The Maximum Limit";
 echo "Time - Username - Number of Multilogin"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 cat /root/log-limit.txt
 else
 echo " No user has committed a violation"
@@ -81,7 +81,7 @@ echo " "
 echo " The user-limit script not been executed."
 fi
 echo " ";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo " ";
 read -n 1 -s -r -p "Press any key to back on menu"
 
